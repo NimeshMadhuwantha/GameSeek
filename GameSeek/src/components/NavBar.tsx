3d3a8d5a666e93d84629969e97a2865f53232239
NimeshMadhuwantha <janmadhuwantha@std.appsc.sab.ac.lk>
2024-07-22 11:19:46 +0530

    update

---
+++ GameSeek/src/components/NavBar.tsx
@@ -0,0 +1,12 @@
+import { HStack, Image } from '@chakra-ui/react'
+import React from 'react'
+
+const NavBar = () => {
+  return (
+    <HStack>
+        <Image src={}/>
+    </HStack>
+  )
+}
+
+export default NavBar
