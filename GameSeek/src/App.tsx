bacc42301d440ade4217df59fe5f1444b77aae9b
NimeshMadhuwantha <janmadhuwantha@std.appsc.sab.ac.lk>
2024-07-22 12:45:39 +0530

    update NavBar

--- GameSeek/src/App.tsx
+++ GameSeek/src/App.tsx
@@ -1,4 +1,5 @@
 import { Grid, GridItem, Show } from "@chakra-ui/react"
+import NavBar from "./components/NavBar";
 
 
 function App() {
@@ -7,11 +8,17 @@ return (
   base: '"nav" "main"',
   lg: '"nav nav" "aside main"'
 }}>
-  <GridItem area='nav' bg='coral'>Nav</GridItem>
+  <GridItem area='nav' >
+    <NavBar/>
+    </GridItem>
    <Show above="lg"> 
-  <GridItem area='aside' bg='gold'>Aside</GridItem>
+  <GridItem area='aside' bg='gold'>
+    Aside
+    </GridItem>
   </Show>
-  <GridItem area='main' bg='dodgerblue'>Main</GridItem>
+  <GridItem area='main' bg='dodgerblue'>
+    Main
+    </GridItem>
 
 
 </Grid>
--- GameSeek/src/components/NavBar.tsx
+++ GameSeek/src/components/NavBar.tsx
@@ -1,10 +1,13 @@
-import { HStack, Image } from '@chakra-ui/react'
+import { HStack, Image, Text } from '@chakra-ui/react'
 import React from 'react'
+import logo from '../assets/logo.png';
+import { px } from 'framer-motion';
 
 const NavBar = () => {
   return (
     <HStack>
-        <Image src={}/>
+        <Image src={logo} boxSize='60px'/>
+        <Text> NavBar</Text>
     </HStack>
   )
 }
