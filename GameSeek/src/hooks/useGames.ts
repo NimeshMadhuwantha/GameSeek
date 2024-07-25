6479ad32a1e3892afef2f74e9734c2f0ddeb0c37
NimeshMadhuwantha <janmadhuwantha@std.appsc.sab.ac.lk>
2024-07-25 14:00:57 +0530

    creating game cards

--- GameSeek/src/hooks/useGames.ts
+++ GameSeek/src/hooks/useGames.ts
@@ -6,9 +6,10 @@ import { CanceledError } from "axios";
 
 
 
-interface Game{
+export interface Game{
     id: number;
     name: string;
+    background_image: string;
 }
 
 interface FetchGamesResponse{
