eaf78fed1e5f4f1e98aba1f721879712bbf7b91c
NimeshMadhuwantha <janmadhuwantha@std.appsc.sab.ac.lk>
2024-07-28 08:27:51 +0530

    show gamecard skeleton

--- GameSeek/src/hooks/useGames.ts
+++ GameSeek/src/hooks/useGames.ts
@@ -26,24 +26,31 @@ interface FetchGamesResponse{
 const useGames =() => {
     const [games, setGames] = useState<Game[]>([]);
     const [error, setError] = useState('');
+    const [isLoading, setLoading] = useState(false);
 
     useEffect(() => {
+
+        setLoading(true);
         const controller = new AbortController();
 
 
         apiClient
           .get<FetchGamesResponse>("/games",{signal: controller.signal})
-          .then((res) => setGames(res.data.results))
+          .then((res) => {
+            setGames(res.data.results);
+            setLoading(false);
+        })
           .catch((err) => {
             if (err instanceof CanceledError) return;
-            setError(err.message)
+            setError(err.message);
+            setLoading(false);
         }); 
 
         return () => controller.abort();
 
     }, []);
 
-    return { games, error};
+    return { games, error, isLoading};
 
 }
 
