4674a372eed0a3cb74f31672ee84d2475ea4238c
NimeshMadhuwantha <janmadhuwantha@std.appsc.sab.ac.lk>
2024-07-28 07:54:05 +0530

    optimized images

--- GameSeek/src/components/GameCard.tsx
+++ GameSeek/src/components/GameCard.tsx
@@ -3,6 +3,7 @@ import { Game } from '../hooks/useGames'
 import { Card, CardBody, Heading, HStack, Image, Text } from '@chakra-ui/react'
 import PlatformIconList from './PlatformIconList'
 import CriticScore from './CriticScore'
+import getCroppedImageUrl from '../services/image-url'
 
 
 
@@ -13,7 +14,7 @@ interface Props {
 const GameCard = ({ game }: Props) => {
   return (
     <Card borderRadius={10} overflow='hidden'>
-      <Image src={game.background_image}/>
+      <Image src={getCroppedImageUrl(game.background_image)}/>
       <CardBody>
         <Heading fontSize='2xl'>{game.name}</Heading>
         <HStack justifyContent='space-between'>
