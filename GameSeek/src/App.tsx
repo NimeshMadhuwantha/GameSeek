cde783c219dcaf61cd8cb67ff1232200a385f958
NimeshMadhuwantha <janmadhuwantha@std.appsc.sab.ac.lk>
2024-07-31 12:06:17 +0530

    higlight the selected genre

--- GameSeek/src/App.tsx
+++ GameSeek/src/App.tsx
@@ -23,7 +23,7 @@ templateColumns={{
     </GridItem>
    <Show above="lg"> 
   <GridItem area='aside' paddingX={5} >
-    <GenreList onSelectGenre={(genre) => setSelectedGenre(genre)} />
+    <GenreList selectedGenre={selectedGenre} onSelectGenre={(genre) => setSelectedGenre(genre)} />
     </GridItem>
   </Show>
   <GridItem area='main' >
