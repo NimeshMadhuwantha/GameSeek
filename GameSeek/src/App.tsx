e4b9bd794f4d8543a1303342f577cf87f6cf8fef
NimeshMadhuwantha <janmadhuwantha@std.appsc.sab.ac.lk>
2024-07-31 12:26:53 +0530

    build platform selector

--- GameSeek/src/App.tsx
+++ GameSeek/src/App.tsx
@@ -4,6 +4,7 @@ import GameGrid from "./components/GameGrid";
 import GenreList from "./components/GenreList";
 import { useState } from "react";
 import { Genre } from "./hooks/useGenres";
+import PlatformSelector from "./components/PlatformSelector";
 
 
 function App() {
@@ -27,6 +28,7 @@ templateColumns={{
     </GridItem>
   </Show>
   <GridItem area='main' >
+    <PlatformSelector/>
     <GameGrid selectedGenre={selectedGenre}/>
     </GridItem>
 
