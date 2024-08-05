600dfcd462ce25d126e51d960d40f72b4937b349
NimeshMadhuwantha <janmadhuwantha@std.appsc.sab.ac.lk>
2024-08-05 21:56:18 +0530

    sort the games

--- GameSeek/src/components/SortSelector.tsx
+++ GameSeek/src/components/SortSelector.tsx
@@ -1,19 +1,33 @@
 import { Button, Menu, MenuButton, MenuItem, MenuList } from '@chakra-ui/react'
 import { BsChevronBarDown } from 'react-icons/bs'
 
-const SortSelector = () => {
+interface Props{
+    onSelectSortOrder:(sortOrder:string) => void;
+    sortOrder: string;
+}
+
+const SortSelector = ({onSelectSortOrder, sortOrder}:Props) => {
+    const sortOrders = [
+        { value: '', label: 'Relevance'},
+        { value: '-added', label: 'Date added'},
+        { value: 'name', label: 'Name'},
+        { value: '-released', label: 'Release date'},
+        { value: 'metacritic', label: 'Popularity'},
+        { value: '-rating', label: 'Average rating'},
+    ];
+
+    const currentSortOrder = sortOrders.find(order => order.value === sortOrder);
     return (
         <Menu>
             <MenuButton as={Button} rightIcon={<BsChevronBarDown/>} >
-            Order by : Relevence
+            Order by : {currentSortOrder?.label || 'Relevance'}
             </MenuButton>
             <MenuList>
-                <MenuItem>Relevence</MenuItem>
-                <MenuItem>Date added</MenuItem>
-                <MenuItem>Name</MenuItem>
-                <MenuItem>Release date</MenuItem>
-                <MenuItem>Popularity</MenuItem>
-                <MenuItem>Average rating</MenuItem>
+                {sortOrders.map(order => (
+                    <MenuItem onClick={()=> onSelectSortOrder(order.value)} key ={order.value} value={order.value}>
+                        {order.label}
+                    </MenuItem>
+                    ))}
             </MenuList>
         </Menu>
       )
--- GameSeek/src/hooks/useGames.ts
+++ GameSeek/src/hooks/useGames.ts
@@ -24,7 +24,8 @@ const useGames =(gameQuery: GameQuery) =>
         '/games', 
         {params: {
     genres: gameQuery.genre?.id, 
-    platforms: gameQuery.platform?.id
+    platforms: gameQuery.platform?.id,
+    ordering: gameQuery.sortOrder
 }
 }, 
 [gameQuery]
