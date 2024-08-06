8cbb97992f7dcf037f633fdc74381b350a42cbf5
NimeshMadhuwantha <janmadhuwantha@std.appsc.sab.ac.lk>
2024-08-06 14:07:23 +0530

    create search input

--- GameSeek/src/components/NavBar.tsx
+++ GameSeek/src/components/NavBar.tsx
@@ -3,11 +3,13 @@ import React from 'react'
 import logo from '../assets/logo.png';
 import { px } from 'framer-motion';
 import ColorModeSwitch from './ColorModeSwitch';
+import SearchInput from './SearchInput';
 
 const NavBar = () => {
   return (
-    <HStack justifyContent={'space-between'} padding='10px'>
+    <HStack  padding='10px'>
         <Image src={logo} boxSize='60px'/>
+        <SearchInput/>
         <ColorModeSwitch/>
     </HStack>
   )
